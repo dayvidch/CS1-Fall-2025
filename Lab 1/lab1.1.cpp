8f55afff2a348a68c380ceb803bb5e1dc99fd629
dayvidch <davidcha2006@gmail.com>
2026-03-06 14:50:15 -0800

    Lab 1

---
+++ Lab 1/lab1.1.cpp
@@ -0,0 +1,10 @@
+// This is the first program that just writes out a simple message
+// Place your name here
+#include <iostream>
+using namespace std;
+int main (){
+    cout << "Now is the time for all good men" << endl;
+    cout << "To come to the aid of their party" << endl;
+    return 0;
+
+}
