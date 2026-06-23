fae10fafa0ee38406bd3b4dfafc555e4789070da
dayvidch <davidcha2006@gmail.com>
2026-06-22 17:57:42 -0700

    Lab 6

---
+++ Lab 06/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+ert cents
+// to dollars and cents which is stored in a local variable called
+// total which is sent back to the calling function through the
+// parameter dollars. It will keep a running total of all the m
